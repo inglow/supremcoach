58cacd8cafba8c713e02d93be82dcea87fe47d18
inglow <jouanny.jerome@gmail.com>
2016-03-07 10:44:26 +0100

    code ordi fixe
    l’avancement

--- src/controleur/Personne.java
+++ src/controleur/Personne.java
@@ -1,5 +1,8 @@
 package controleur;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Personne 
 {
@@ -28,7 +31,23 @@ public class Personne
        this.adresse = adresse;
        this.email = email;
    }
-  
+   
+   public static String getMD5(String input) {
+       try {
+           MessageDigest md = MessageDigest.getInstance("MD5");
+           byte[] messageDigest = md.digest(input.getBytes());
+           BigInteger number = new BigInteger(1, messageDigest);
+           String hashtext = number.toString(16);
+           // Now we need to zero pad it if you actually want the full 32 chars.
+           while (hashtext.length() < 32) {
+               hashtext = "0" + hashtext;
+           }
+           return hashtext;
+       }
+       catch (NoSuchAlgorithmException e) {
+           throw new RuntimeException(e);
+       }
+   }
    
    public String toString()
    {
--- src/vue/VuePersonne.java
+++ src/vue/VuePersonne.java
@@ -4,6 +4,7 @@ import controleur.Personne;
 import controleur.Produit;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,9 +12,11 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.LinkedList;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -22,6 +25,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.text.AbstractDocument.LeafElement;
+
 import modele.Modele;
 import modele.ModelePersonne;
 
@@ -33,13 +38,14 @@ public class VuePersonne extends JFrame implements ActionListener {
     private JPanel panelLister = new JPanel();
     private JPanel panelRechercher = new JPanel();
     private JPanel panelSupprimer = new JPanel();
-
+    private JPanel  paneltitre= new JPanel();
+    private JPanel  panelfooter= new JPanel();
     //Construction des objets du Panel Menu
     private JButton btAjouter = new JButton(new ImageIcon("add_user.png" ));
-    private JButton btLister = new JButton("Lister");
-    private JButton btRechercher = new JButton("Rechercher");
-    private JButton btSupprimer = new JButton("Supprimer");
-    private JButton btQuitter = new JButton("Quitter");
+    private JButton btLister = new JButton(new ImageIcon("edituser.png" ));
+    private JButton btRechercher = new JButton(new ImageIcon("recherche-icon.png" ));
+    private JButton btSupprimer = new JButton(new ImageIcon("delete-user.png" ));
+    private JButton btQuitter = new JButton(new ImageIcon("croix.png" ));
     
     //construction des objets du panel Ajouter
     private JButton btAnnuler = new JButton("Annuler");
@@ -69,28 +75,72 @@ public class VuePersonne extends JFrame implements ActionListener {
     private JTextField tfSupprimer = new JTextField("");
     private JButton btSuppr = new JButton("Supprimer");
     private JLabel lbSupprimer = new JLabel("Nombre de personne suppriméees : ");
+    //contruction du pannel titre
+    private JLabel lbtitre1 = new JLabel("Gestion des clients");
+  //contruction du pannel footer
+    private JLabel lbfooter = new JLabel("Supremcoach tous droits réservés  ©2013-2015");
+    private JButton btRetour = new JButton(new ImageIcon("Accueil.png" ));
+    private JButton btCoachSu = new JButton(new ImageIcon("coooachsuprem.png" ));
     
     
-
+    
     public VuePersonne() {
-        this.setTitle("Gestion de Personne");
-        this.setBounds(150, 150, 1000, 600);
+        this.setTitle("Gestion des Clients");
+        this.setBounds(0, 150, 1200, 750);
         this.setResizable(false);
         this.setLayout(null);
+      
+        Font fonte = new Font("TimesRoman ",Font.BOLD,30);
+      
+        this.paneltitre.setBounds(0, 0, 1200, 100);
+        this.paneltitre.setBackground(new Color(10,125,255));
+        this.paneltitre.setLayout(null);
+        this.paneltitre.add(this.lbtitre1);
+        this.lbtitre1.setFont(fonte);
+        this.lbtitre1.setBounds(475, 20, 800, 50);
+        this.lbtitre1.setForeground(Color.WHITE);
+     
+        this.paneltitre.add(this.btRetour);
+        this.paneltitre.add(this.btCoachSu);
+        this.btCoachSu.setBounds(35, 15, 135, 80);
+        this.btCoachSu.setOpaque(false);
+        this.btCoachSu.setContentAreaFilled(false);
+        this.btCoachSu.setBorderPainted(false);
+        this.btRetour.setBounds(1075, 0, 100, 100);
+        this.btRetour.setOpaque(false);
+        this.btRetour.setContentAreaFilled(false);
+        this.btRetour.setBorderPainted(false);
+     
+        this.add(this.paneltitre);
+        this.panelfooter.add(this.lbfooter);
+        this.panelfooter.setFont(fonte);
+        this.panelfooter.setForeground(Color.WHITE);
+        this.panelfooter.setBounds(0, 700, 1200, 50);
+        this.panelfooter.setBackground(new Color(10,125,255));
 
+
+        this.add(this.panelfooter);
         //Construiction du panel Menu
-        this.panelMenu.setBounds(10, 10, 130, 340);
-        this.panelMenu.setBackground(Color.red);
-        this.panelMenu.setLayout(new GridLayout(7, 1));
-        this.panelMenu.add(new JLabel("Menu Principal"));
+        this.panelMenu.setBounds(0, 100, 210, 650);
+        this.panelMenu.setBackground(new Color(10,125,255));
         this.panelMenu.add(this.btAjouter);
         this.panelMenu.add(this.btRechercher);
         this.panelMenu.add(this.btSupprimer);
         this.panelMenu.add(this.btLister);
         this.panelMenu.add(this.btQuitter);
+     
         this.btAjouter.setOpaque(false);
         this.btAjouter.setContentAreaFilled(false);
         this.btAjouter.setBorderPainted(false);
+        this.btRechercher.setOpaque(false);
+        this.btRechercher.setContentAreaFilled(false);
+        this.btRechercher.setBorderPainted(false);
+        this.btSupprimer.setOpaque(false);
+        this.btSupprimer.setContentAreaFilled(false);
+        this.btSupprimer.setBorderPainted(false);
+        this.btLister.setOpaque(false);
+        this.btLister.setContentAreaFilled(false);
+        this.btLister.setBorderPainted(false);
         this.panelMenu.add(new JLabel(""));
         this.panelMenu.setVisible(true);
         this.add(this.panelMenu);
@@ -106,77 +156,82 @@ public class VuePersonne extends JFrame implements ActionListener {
         this.btFermer.addActionListener(this);
         this.btOk.addActionListener(this);
         this.btSuppr.addActionListener(this);
-
+        this.btRetour.addActionListener(this);
         //Construction du panel ajouter 
-        this.panelAjouter.setBounds(140, 10, 350, 340);
-        this.panelAjouter.setBackground(Color.cyan);
-        this.panelAjouter.setLayout(new GridLayout(7, 2));
-       
-        this.panelAjouter.add(new JLabel("Prénom : "));
+        this.panelAjouter.setBounds(210, 100, 1000, 600);
+        this.panelAjouter.setBackground(new Color(88,167,255));
+        
+        
+        this.panelAjouter.setLayout(new GridLayout(10, 2));
+        this.panelAjouter.add(new JLabel("Ajouter un client"));
+        this.panelAjouter.add(new JLabel(""));
+        this.panelAjouter.add(new JLabel(" Prenom : "));
         this.panelAjouter.add(this.tfPrenom);
-        this.panelAjouter.add(new JLabel("Pseudo : "));
+        this.panelAjouter.add(new JLabel(" Pseudo : "));
         this.panelAjouter.add(this.tfPseudo);
-        this.panelAjouter.add(new JLabel("Mdp : "));
+        this.panelAjouter.add(new JLabel(" Mdp : "));
         this.panelAjouter.add(this.tfMdp);
-        this.panelAjouter.add(new JLabel("Nom : "));
+        this.panelAjouter.add(new JLabel(" Nom : "));
         this.panelAjouter.add(this.tfNom);
-        this.panelAjouter.add(new JLabel("Cp : "));
+        this.panelAjouter.add(new JLabel(" Cp : "));
         this.panelAjouter.add(this.tfCp);
-        this.panelAjouter.add(new JLabel("Adresse : "));
+        this.panelAjouter.add(new JLabel(" Adresse : "));
         this.panelAjouter.add(this.tfAdresse);
-        this.panelAjouter.add(new JLabel("Email : "));
+        this.panelAjouter.add(new JLabel(" Email : "));
         this.panelAjouter.add(this.tfEmail);
        
-        this.panelAjouter.add(new JLabel("Telephone : "));
+        this.panelAjouter.add(new JLabel(" Telephone : "));
         this.panelAjouter.add(this.tfTel);
         this.panelAjouter.add(this.btAnnuler); 
 
         this.panelAjouter.add(this.btEnregistrer);
       
-        this.panelAjouter.add(new JLabel(""));
-        this.panelAjouter.add(new JLabel(""));
+       
         this.panelAjouter.setVisible(false);
         this.add(this.panelAjouter);
 
         //Construction du panel lister
-        this.panelLister.setBounds(140, 10, 500, 400);
-        this.panelLister.setBackground(Color.yellow);
+        this.panelLister.setBounds(210, 100, 1000, 600);
+        this.panelLister.setBackground(new Color(88,167,255));
         this.panelLister.setLayout(null);
-        this.lbLister.setBounds(100, 20, 200, 20);
+        this.lbLister.setBounds(100, 20, 600, 50);
         this.panelLister.add(this.lbLister);
+        this.lbLister.setFont(fonte);
         this.btFermer.setBounds(200, 300, 200, 20);
         this.panelLister.add(this.btFermer);
         this.panelLister.setVisible(false);
         this.add(this.panelLister);
 
         //Construction du panel rechercher
-        this.panelRechercher.setBounds(140, 10, 500, 340);
-        this.panelRechercher.setBackground(Color.green);
+        this.panelRechercher.setBounds(210, 100, 1000, 600);
+        this.panelRechercher.setBackground(new Color(88,167,255));
         this.panelRechercher.setLayout(null);
-        this.lbRechercher.setBounds(100, 20, 200, 20);
+        this.lbRechercher.setBounds(250, 40, 600, 40);
+        this.lbRechercher.setFont(fonte);
         this.panelRechercher.add(this.lbRechercher);
-        this.cbxPersonne.setBounds(50, 80, 200, 20);
+        this.cbxPersonne.setBounds(165, 150, 600, 20);
         this.panelRechercher.add(this.cbxPersonne);
-        this.btOk.setBounds(260, 80, 80, 20);
+        this.btOk.setBounds(765, 150, 80, 20);
         this.panelRechercher.add(this.btOk);
-        this.taResultat.setBounds(100, 120, 200, 150);
+        this.taResultat.setBounds(165, 190, 680, 300);
         this.panelRechercher.add(this.taResultat);
         this.taResultat.setEditable(false);
-        this.taResultat.setBackground(Color.GREEN);
+ 
         this.panelRechercher.setVisible(false);
         this.add(this.panelRechercher);
 
         //Construction du panel supprimer 
-        this.panelSupprimer.setBounds(140, 10, 500, 340);
-        this.panelSupprimer.setBackground(Color.pink);
+        this.panelSupprimer.setBounds(210, 100, 1000, 600);
+        this.panelSupprimer.setBackground(new Color(88,167,255));
         this.panelSupprimer.setLayout(null);
-        this.lbSuppr.setBounds(100, 20, 200, 20);
+        this.lbSuppr.setBounds(300, 40, 600, 40);
+        this.lbSuppr.setFont(fonte);
         this.panelSupprimer.add(this.lbSuppr);
-        this.tfSupprimer.setBounds(50, 80, 200, 20);
+        this.tfSupprimer.setBounds(140, 150, 600, 20);
         this.panelSupprimer.add(this.tfSupprimer);
-        this.btSuppr.setBounds(300, 80, 100, 20);
+        this.btSuppr.setBounds(740, 150, 120, 20);
         this.panelSupprimer.add(this.btSuppr);
-        this.lbSupprimer.setBounds(120, 130, 250, 20);
+        this.lbSupprimer.setBounds(140, 190, 500, 50);
         this.panelSupprimer.add(this.lbSupprimer);
         
         this.panelSupprimer.setVisible(false);
@@ -230,7 +285,7 @@ public class VuePersonne extends JFrame implements ActionListener {
                 i++;
             }
             this.tabPersonne = new JTable(donnees, titres);
-     
+          
             //ajouter a la table une action Mouslistener 
             this.tabPersonne.addMouseListener(new MouseListener() {
 				
@@ -244,8 +299,7 @@ public class VuePersonne extends JFrame implements ActionListener {
 				public void mousePressed(MouseEvent arg0) {
 					// ici on recupere les donnŽ de la ligne selecter on crŽe le produit on levoie popup
 					int a =tabPersonne.getSelectedRow();
-					try 
-					{
+					
 					int id=Integer.parseInt(tabPersonne.getValueAt(a,0).toString());
 					String prenom=tabPersonne.getValueAt(a,1).toString();
 					String nom=tabPersonne.getValueAt(a,2).toString();
@@ -259,11 +313,7 @@ public class VuePersonne extends JFrame implements ActionListener {
 					
 					Personne unePersonne = new Personne(id, prenom, nom, cp, telephone, pseudo, mdp, avatar, adresse, email);
 					new VueEdition(unePersonne);
-					}
-					catch(NumberFormatException exp)
-					{
-						
-					}
+				
 					
 				}
 				
@@ -318,23 +368,59 @@ public class VuePersonne extends JFrame implements ActionListener {
             this.tfCp.setText("");
             
         } else if (e.getSource() == this.btEnregistrer) {
-            
+        	boolean ok = true;
             String pseudo = this.tfPseudo.getText();
             String prenom = this.tfPrenom.getText();
             String nom = this.tfNom.getText();
             String email = this.tfEmail.getText();
             String adresse = this.tfAdresse.getText();
             String mdp = this.tfMdp.getText();
-            String cp=this.tfCp.getText();
-            String tel=this.tfTel.getText();
+            int tel=0;
+            int cp=0;
+            try{
+            	 tel=Integer.parseInt(this.tfTel.getText());
+            	
+            }
+            catch(NumberFormatException exp)
+            {
+            	   ok = false;
+                   JOptionPane.showMessageDialog(this, "Erreur de saisie du Téléphone");
+                   this.tfTel.setBackground(Color.red);
+            }
+          
+            try{
+           	 cp=Integer.parseInt(this.tfCp.getText());
+           }
+           catch(NumberFormatException exp)
+           {
+           	   ok = false;
+                  JOptionPane.showMessageDialog(this, "Erreur de saisie du Code-Postal");
+                  this.tfCp.setBackground(Color.red);
+           }
+           
+       String tel2=Integer.toString(tel);
+       
+       if(tel2.length()!=9)
+       {
+    	   ok = false;
+           JOptionPane.showMessageDialog(this, "Erreur du nombre de chiffre du telephone");
+           this.tfTel.setBackground(Color.red);
+       }
+       String cp2=Integer.toString(cp);
+       if(cp2.length()>5)
+       {
+    	   ok = false;
+           JOptionPane.showMessageDialog(this, "Erreur du nombre de chiffre du Cp"+cp2.length());
+           this.tfCp.setBackground(Color.red);
+       }
             int id = 0;
          String avatar="pas d'avatar";
-            boolean ok = true;
+          String mdp2=Personne.getMD5(mdp);
           
           
             if (ok == true) {
               
-                Personne unePersonne = new Personne(id, prenom, nom, cp, tel, pseudo, mdp, avatar, adresse, email);
+                Personne unePersonne = new Personne(id, prenom, nom, cp2, tel2, pseudo, mdp2, avatar, adresse, email);
                 ModelePersonne.insertpersonne(unePersonne);
                 JOptionPane.showMessageDialog(this, "Personne inséré avec succes");
                 this.panelAjouter.setVisible(false);
@@ -347,7 +433,7 @@ public class VuePersonne extends JFrame implements ActionListener {
         } else if (e.getSource() == this.btFermer) {
             this.panelLister.setVisible(false);
         } else if (e.getSource() == this.btOk) {
-            String chaine = this.cbxPersonne.getSelectedItem().toString();
+        	String chaine = this.cbxPersonne.getSelectedItem().toString();
             String tab[] = new String[10];
             tab = chaine.split("-");
             chaine = "Les clients recherchés"
@@ -368,6 +454,10 @@ public class VuePersonne extends JFrame implements ActionListener {
             String cle = this.tfSupprimer.getText();
             int nb = ModelePersonne.deletepersonnes(cle);
             this.lbSupprimer.setText("Nombre de client supprimé : "+nb);
+        }
+        else if (e.getSource() == this.btRetour)
+        {
+        	new VuePersonne();
         }
     }
 
